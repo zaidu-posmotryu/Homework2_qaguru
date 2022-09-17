b4bdaa7d7391d2cfb591fc7f47688f4aa0bc6c4b
zaidu-posmotryu <cherchimda@gmail.com>
2022-09-17 20:42:05 +0400

    fix logic

--- src/test/java/homework/ExampleTest.java
+++ src/test/java/homework/ExampleTest.java
@@ -3,15 +3,17 @@ package homework;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
